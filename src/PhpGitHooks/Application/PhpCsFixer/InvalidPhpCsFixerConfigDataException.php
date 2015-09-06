860e12b8ae1e5fce3b06bc2364c672d6d6a3f44a
Pablo Braulio <brulics@gmail.com>
2015-09-06 18:05:51 +0200

    Format exception.

--- src/PhpGitHooks/Application/PhpCsFixer/InvalidPhpCsFixerConfigDataException.php
+++ src/PhpGitHooks/Application/PhpCsFixer/InvalidPhpCsFixerConfigDataException.php
@@ -4,6 +4,6 @@ namespace PhpGitHooks\Application\PhpCsFixer;
 
 final class InvalidPhpCsFixerConfigDataException extends \Exception
 {
-    protected $message = 'Invalid entry for php-cs-fixer in your php-git-hooks.php file.'."\n".
+    protected $message = 'Invalid entry for php-cs-fixer in your php-git-hooks.php file.' . "\n" .
     'Please remove php-cs-fixer entry and execute composer install.';
 }
