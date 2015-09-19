7b74e173fc83350f4f04333a01bc339e7b8af55e
Pablo Braulio <brulics@gmail.com>
2015-09-19 18:04:23 +0200

    set permissions in copy hook file.

--- src/PhpGitHooks/Infrastructure/Git/HooksFileCopier.php
+++ src/PhpGitHooks/Infrastructure/Git/HooksFileCopier.php
@@ -13,14 +13,17 @@ class HooksFileCopier
 
     /**
      * @param string $hook
-     * @param bool   $enabled
+     * @param bool $enabled
      */
     public function copy($hook, $enabled)
     {
         if (true === $enabled) {
-            if (false === file_exists(self::GIT_HOOKS_PATH.$hook)) {
-                $copy = new Process('cp '.__DIR__.'/../../../../hooks/'.$hook.' .git/hooks/'.$hook);
+            if (false === file_exists(self::GIT_HOOKS_PATH . $hook)) {
+                $copy = new Process('cp ' . __DIR__ . '/../../../../hooks/' . $hook . self::GIT_HOOKS_PATH . $hook);
                 $copy->run();
+
+                $permissions = new Process('chmod 775 ' . self::GIT_HOOKS_PATH . $hook);
+                $permissions->run();
             }
         }
     }
