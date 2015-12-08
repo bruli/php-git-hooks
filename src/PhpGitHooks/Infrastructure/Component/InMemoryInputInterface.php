258ea46f5a53a62f1740baf6b813ac14df4e96d9
Pablo Braulio <brulics@gmail.com>
2015-12-08 20:30:02 +0100

    Update interface

--- src/PhpGitHooks/Infrastructure/Component/InMemoryInputInterface.php
+++ src/PhpGitHooks/Infrastructure/Component/InMemoryInputInterface.php
@@ -24,35 +24,6 @@ class InMemoryInputInterface implements InputInterface
     }
 
     /**
-     * Returns true if the raw parameters (not parsed) contain a value.
-     *
-     * This method is to be used to introspect the input parameters
-     * before they have been validated. It must be used carefully.
-     *
-     * @param string|array $values The values to look for in the raw parameters (can be an array)
-     *
-     * @return bool true if the value is contained in the raw parameters
-     */
-    public function hasParameterOption($values)
-    {
-    }
-
-    /**
-     * Returns the value of a raw option (not parsed).
-     *
-     * This method is to be used to introspect the input parameters
-     * before they have been validated. It must be used carefully.
-     *
-     * @param string|array $values  The value(s) to look for in the raw parameters (can be an array)
-     * @param mixed        $default The default value to return if no result is found
-     *
-     * @return mixed The option value
-     */
-    public function getParameterOption($values, $default = false)
-    {
-    }
-
-    /**
      * Binds the current Input instance with the given arguments and options.
      *
      * @param InputDefinition $definition A InputDefinition instance
@@ -182,5 +153,36 @@ class InMemoryInputInterface implements InputInterface
     public function setFirstArgument($firstArgument)
     {
         $this->firstArgument = $firstArgument;
+    }
+
+    /**
+     * Returns true if the raw parameters (not parsed) contain a value.
+     *
+     * This method is to be used to introspect the input parameters
+     * before they have been validated. It must be used carefully.
+     *
+     * @param string|array $values     The values to look for in the raw parameters (can be an array)
+     * @param bool         $onlyParams Only check real parameters, skip those following an end of options (--) signal
+     *
+     * @return bool true if the value is contained in the raw parameters
+     */
+    public function hasParameterOption($values, $onlyParams = false)
+    {
+    }
+
+    /**
+     * Returns the value of a raw option (not parsed).
+     *
+     * This method is to be used to introspect the input parameters
+     * before they have been validated. It must be used carefully.
+     *
+     * @param string|array $values     The value(s) to look for in the raw parameters (can be an array)
+     * @param mixed        $default    The default value to return if no result is found
+     * @param bool         $onlyParams Only check real parameters, skip those following an end of options (--) signal
+     *
+     * @return mixed The option value
+     */
+    public function getParameterOption($values, $default = false, $onlyParams = false)
+    {
     }
 }
