033951889f4eb9ccc64c6b7e58abd5c759eb0eab
Pablo Braulio <brulics@gmail.com>
2016-03-08 15:38:22 +0100

    Fix phpcsfixer exception

--- src/PhpGitHooks/Infrastructure/PhpCsFixer/PhpCsFixerHandler.php
+++ src/PhpGitHooks/Infrastructure/PhpCsFixer/PhpCsFixerHandler.php
@@ -4,6 +4,7 @@ namespace PhpGitHooks\Infrastructure\PhpCsFixer;
 
 use IgnoreFiles\IgnoreFiles;
 use PhpGitHooks\Application\Message\MessageConfigData;
+use PhpGitHooks\Command\BadJobLogo;
 use PhpGitHooks\Command\OutputHandlerInterface;
 use PhpGitHooks\Infrastructure\Common\InteractiveToolInterface;
 use PhpGitHooks\Infrastructure\Common\ToolHandler;
@@ -75,10 +76,11 @@ class PhpCsFixerHandler extends ToolHandler implements InteractiveToolInterface,
                 }
 
                 if ($errors) {
-                    $this->writeOutputError(
-                        new PhpCsFixerException(implode('', $errors)),
-                        $messages[MessageConfigData::KEY_ERROR_MESSAGE]
-                    );
+                    $this->outputHandler->setError($this->getErrors($errors));
+                    $this->output->writeln($this->outputHandler->getError());
+                    $this->output->writeln(BadJobLogo::paint($messages[MessageConfigData::KEY_ERROR_MESSAGE]));
+
+                    throw  new PhpCsFixerException();
                 }
 
                 $this->output->writeln($this->outputHandler->getSuccessfulStepMessage());
@@ -115,5 +117,21 @@ class PhpCsFixerHandler extends ToolHandler implements InteractiveToolInterface,
     public function setLevels(array $levels)
     {
         $this->levels = $levels;
+    }
+
+    /**
+     * @param array $errors
+     *
+     * @return null|string
+     */
+    private function getErrors(array $errors)
+    {
+        $errorText = null;
+
+        foreach ($errors as $error) {
+            $errorText .= trim($error)."\n";
+        }
+
+        return $errorText;
     }
 }
