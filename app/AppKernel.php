4b02d18e3d82871ce950193e17043d68b7a191d0
Pablo Braulio <brulics@gmail.com>
2016-06-15 18:07:10 +0200

    Add AppKernel

---
+++ app/AppKernel.php
@@ -0,0 +1,43 @@
+<?php
+
+use PhpGitHooks\Infrastructure\CommandBus\CommandBusCompilerPass;
+use PhpGitHooks\Infrastructure\QueryBus\QueryBusCompilerPass;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class AppKernel
+{
+    const SERVICES_FILE = 'services.yml';
+    const CONFIG_PATH = '/config/';
+
+    /**
+     * @var ContainerBuilder
+     */
+    private $container;
+
+    public function __construct()
+    {
+        $this->container = new ContainerBuilder();
+        $this->container->addCompilerPass(new \CommandBus\CommandBus\CommandBusCompilerPass());
+        $this->container->addCompilerPass(new \CommandBus\QueryBus\QueryBusCompilerPass());
+        $this->getConfigServices();
+        $this->container->compile();
+    }
+
+    private function getConfigServices()
+    {
+        $loader = new YamlFileLoader($this->container, new FileLocator(__DIR__.self::CONFIG_PATH));
+        $loader->load(self::SERVICES_FILE);
+    }
+
+    /**
+     * @param string $serviceName
+     *
+     * @return object
+     */
+    public function get($serviceName)
+    {
+        return $this->container->get($serviceName);
+    }
+}
