3324abdb6aa25fd5c941690794d85a9a9acb41ee
Pablo Braulio <brulics@gmail.com>
2016-10-22 17:04:39 +0200

    Ignore var dir

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,17 @@ class AppKernel extends Kernel
         ];
     }
 
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
+
+
     /**
      * Loads the container configuration.
      *
