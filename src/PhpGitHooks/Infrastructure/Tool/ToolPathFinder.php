eb5f4921770e20933d2dbc99fda376b4301acfa5
Jaume Casado Ruiz <minterior@gmail.com>
2017-11-13 17:34:59 +0100

    Replace deprecated dirname(__FILE__) by __DIR__

--- src/PhpGitHooks/Infrastructure/Tool/ToolPathFinder.php
+++ src/PhpGitHooks/Infrastructure/Tool/ToolPathFinder.php
@@ -30,7 +30,7 @@ class ToolPathFinder
             $package = $this->installedPackages[$this->tools[$tool]];
             foreach ($package['bin'] as $bin) {
                 if (preg_match("#${tool}$#", $bin)) {
-                    return dirname(__FILE__).self::COMPOSER_VENDOR_DIR.$package['name'].DIRECTORY_SEPARATOR.$bin;
+                    return __DIR__.self::COMPOSER_VENDOR_DIR.$package['name'].DIRECTORY_SEPARATOR.$bin;
                 }
             }
         }
